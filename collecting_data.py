3b727f43214ef7fe0b4e64482f1a003f017f8bf6
drunkmoriarty <drunkmoriarty@users.noreply.github.com>
2020-10-12 16:47:08 +0200

    display_info()
    Displaying some infos about the account in the terminal

--- collecting_data.py
+++ collecting_data.py
@@ -44,25 +44,34 @@ def get_profil_games(key, id_account):
     info_games=info.json()
     return info_games   
 
-def get_data(info_profil, info_games): 
+def get_data_profil(info_profil): 
     realname=info_profil['response']['players'][0]['realname']
     account_country=info_profil['response']['players'][0]['loccountrycode']
-    return realname, account_country
+    return [realname, account_country]
 
-def display_info(name_account, id_account, realname, account_country): 
+def get_data_profil_games(info_games): 
+    if not info_games['response']:
+        print("This account is private, data couldn't be retrieved.")
+        nb_owned_games= 'Private profil. No information'
+        return nb_owned_games
+    else : 
+        nb_owned_games=info_games['response']['game_count']
+        return nb_owned_games
+
+def display_info(name_account, id_account, realname, account_country, nb_owned_games): 
     print('For the account named ', name_account, ':')
     print('The steam id is ', id_account)
     print('The name of the holder is ', realname)
     print('The account is stated in ', account_country)
+    print('The owner of the account has ', nb_owned_games, ' games on steam')
 
 def process():
     name_account=get_name_account()
     id_account=get_id(name_account)
     info_profil=get_info_profil(key_api, id_account)
+    profil_info=get_data_profil(info_profil)
     info_games=get_profil_games(key_api, id_account)
-    print(info_profil)  
-    print('\n')
-    print(info_games)
-    print('\n')
-
+    owned_games=get_data_profil_games(info_games)
+    display_info(name_account, id_account, profil_info[0], profil_info[1], owned_games)
+    
 process()
